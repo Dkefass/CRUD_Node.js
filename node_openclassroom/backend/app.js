81c5e55bd36258d75b7e67f12c7490e5f28d2288
WKefass <135660527+Dkefass@users.noreply.github.com>
2023-07-22 15:29:30 +0000

    openclassroom

--- node_openclassroom/backend/app.js
+++ node_openclassroom/backend/app.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser=require("body-parser")
-const Thing=require("./models/thing")
+const stuffRoutes=require("./routes/stuff")
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,36 +17,9 @@ mongoose.connect('mongodb+srv://test_node:test_node@cluster0.lxagwwy.mongodb.net
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 app.use(bodyParser.json())
+app.use("/api/stuff",stuffRoutes)
 
 
-app.delete('/api/stuff/:id',(req,res,next)=>{
-  Thing.deleteOne({_id:req.params.id}).then(()=>res.status(200).json({message:"objet supprimé"}))
-  .catch(error=>res.status(400).json({error}))
-})
-app.put('/api/stuff/:id',(req,res,next)=>{
-  Thing.updateOne({_id:req.params.id},{...req.body,_id:req.params.id})
-  .then(()=>res.status(200).json({message:"objet modifié avec sucdès"}))
-  .catch(error=>res.status(400).json({error}))
-})
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id
-    const thing= new Thing({
-        ...req.body
-    })
-    thing.save()
-    .then(()=>res.status(201).json({message:"objet créé"}))
-    .catch(error=>res.status(201).json({error}))
-  })
-
-app.get('/api/stuff/:id',(req,res,next)=>{
-    Thing.findOne({_id:req.params.id}).then(thing=>res.status(200).json(thing))
-    .catch(error=>res.status(404).json({error}))
-  })
-
-app.get('/api/stuff',(req,res,next)=>{
-    Thing.find().then(things=>res.status(200).json(things)).catch(error=>res.status(400).json({error}))
-   
-  })
 
 
 module.exports = app;
---
+++ node_openclassroom/backend/routes/stuff.js
@@ -0,0 +1,35 @@
+const express=require("express")
+const router=express.Router()
+const Thing=require("../models/thing")
+
+
+router.delete('/:id',(req,res,next)=>{
+    Thing.deleteOne({_id:req.params.id}).then(()=>res.status(200).json({message:"objet supprimé"}))
+    .catch(error=>res.staus(400).json({error}))
+  })
+router.put('/:id',(req,res,next)=>{
+    Thing.updateOne({_id:req.params.id},{...req.body,_id:req.params.id})
+    .then(()=>res.status(200).json({message:"objet modifié avec sucdès"}))
+    .catch(error=>res.status(400).json({error}))
+  })
+router.post('/', (req, res, next) => {
+      delete req.body._id
+      const thing= new Thing({
+          ...req.body
+      })
+      thing.save()
+      .then(()=>res.status(201).json({message:"objet créé"}))
+      .catch(error=>res.status(201).json({error}))
+    })
+  
+router.get('/:id',(req,res,next)=>{
+      Thing.findOne({_id:req.params.id}).then(thing=>res.status(200).json(thing))
+      .catch(error=>res.status(404).json({error}))
+    })
+  
+router.get('/',(req,res,next)=>{
+      Thing.find().then(things=>res.status(200).json(things)).catch(error=>res.status(400).json({error}))
+     
+    })
+
+    module.exports=router;
